d1888fac917dcb22828ee7d1672b54cd07b1b8a6
fred <daktarispacetime@gmail.com>
2022-11-29 07:40:54 +0300

    altered grading-calc.js

--- README.md
+++ README.md
@@ -15,14 +15,17 @@ Run the `npm-install` command to install project dependencies. Input and output 
 #Grade Calculator
 
 This calculates the grade according to the student marks values that are input into the program.
+Run with `node grading-calc.js`
 
 #Tax Calculator
 
 This takes a Gross salary input and calculates the Net salary, 
+Run with `tax-calc.js`
 
 #Speed Check calculator
 
 This calculates the value of Driving License demerit points based on how far above the speed limit the driver was driving at.
+Run with `speed.js`
 
 Author : Fred Kamuyu
 
--- grading-calc.js
+++ grading-calc.js
@@ -1,25 +1,25 @@
 // determine the student letter grade
 const prompt = require("prompt-sync")();
-let numberGrade = prompt("Enter Student marks : ")
-let letterGrade = function(numberGrade) {
-    
-  let grade;
-    if (numberGrade >= 79) {
-      grade = 'A';
-    } else if (numberGrade >= 60) {
-      grade = 'B';
-      return letter;
-    } else if (numberGrade >= 70) {
-      grade = 'C';
-    } else if (numberGrade >= 40) {
-      grade = 'D';
-    } else {
-      grade = 'E';
-    }
-    return grade;
-    
-  }
-  console.log(letterGrade());
+//prompts individual to input their marks
+let marks  = +prompt("Enter your marks:")
+//compares the marks entered to the grade ranks
+
+//A > 79, B - 60 to 79, C -  59 to 49, D - 40 to 49, E - less 40
+if(marks>=80 && marks<=100){
+  console.log("A")
+}
+else if(marks>=60 && marks<=79){
+  console.log("B")
+}
+else if(marks>=50 && marks<=59){
+  console.log("C")
+}
+else if(marks>=40 && marks<=49){
+  console.log("D")
+}
+else if(marks>=0 && marks<=39){
+  console.log("E")
+}else console.log("Invalid!!!, Marks should be between 0 and 100")
 
   
   // log grades to terminal
