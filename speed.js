510f72ce280d779dc5a2808cee950087f42d3d29
fred <daktarispacetime@gmail.com>
2022-11-29 08:21:28 +0300

    bug fix on speed.js

--- speed.js
+++ speed.js
@@ -1,4 +1,5 @@
-const prompt = require("prompt-sync")()
+// prompts to input the speed of the car
+const prompt = require("prompt-sync")();
 let speed = +prompt("Enter the speed:")
 //speed limit set to 70 initially and points to start counting from 0
 const limit = 70;
@@ -9,13 +10,11 @@ let demeritPoints = 0;
 //outputs demeritpoints where they are less than or equal 12
 //and licence suspended where demerit points are greater than 12
 if (speed<= limit){
-    console.log("Speed OK")
-}else 
-{
+    console.log("OK")
+}else {
     demeritPoints = (speed - limit)/5
     if (demeritPoints<=12){
-        console.log(`points: ${points}`)
+        console.log(`points: ${demeritPoints}`)
     }
-    else(console.log(`License suspended, ${points} points`))
+    else(console.log(`Licence suspended, ${demeritPoints} points`))
 }
-
